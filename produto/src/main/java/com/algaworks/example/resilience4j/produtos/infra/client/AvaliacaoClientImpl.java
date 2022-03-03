c5f98b20862a00bf991a366fd29bcaa3d21b6614
Alex <alex.silva@algaworks.com.br>
2022-03-03 14:00:42 -0300

    Inicio

--- produto/src/main/java/com/algaworks/example/resilience4j/produtos/infra/client/AvaliacaoClientImpl.java
+++ produto/src/main/java/com/algaworks/example/resilience4j/produtos/infra/client/AvaliacaoClientImpl.java
@@ -2,22 +2,16 @@ package com.algaworks.example.resilience4j.produtos.infra.client;
 
 import com.algaworks.example.resilience4j.produtos.client.avaliacoes.AvaliacaoClient;
 import com.algaworks.example.resilience4j.produtos.client.avaliacoes.AvaliacaoModel;
-import io.github.resilience4j.circuitbreaker.CircuitBreaker;
-import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.decorators.Decorators;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
-import java.time.Duration;
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Supplier;
 
 @Component
 public class AvaliacaoClientImpl implements AvaliacaoClient {
@@ -30,39 +24,14 @@ public class AvaliacaoClientImpl implements AvaliacaoClient {
 			.queryParam("produtoId", "{produtoId}")
 			.encode()
 			.toUriString();
-
-	private final CircuitBreaker circuitBreaker;
-	private final Map<Long, List<AvaliacaoModel>> CACHE = new HashMap<>();
 	
 	public AvaliacaoClientImpl(RestTemplate restTemplate) {
 		this.restTemplate = restTemplate;
-
-		CircuitBreakerConfig config = CircuitBreakerConfig.custom()
-				.failureRateThreshold(50)
-				.slidingWindowSize(8)
-				.minimumNumberOfCalls(4)
-				.permittedNumberOfCallsInHalfOpenState(5)
-				.waitDurationInOpenState(Duration.ofMinutes(1))
-				.build();
-		
-		this.circuitBreaker = CircuitBreaker.of("avaliacaoCB", config);
-		
-		this.circuitBreaker.getEventPublisher()
-				.onStateTransition(event -> {
-					logger.info(event.toString().substring(53));
-				});
 	}
 
 	@Override
-	//@CircuitBreaker(fallbackMethod = "buscarTodosPorProdutoNoCache", name = "avaliacaoCB")
 	public List<AvaliacaoModel> buscarTodosPorProduto(Long produtoId) {
-		Supplier<List<AvaliacaoModel>> buscaAvaliacaoSupplier = Decorators
-				.ofSupplier(() -> executarRequisicao(produtoId))
-				.withCircuitBreaker(circuitBreaker)
-				.withFallback(List.of(Exception.class), e -> this.buscarTodosPorProdutoNoCache(produtoId, e))
-				.decorate();
-
-		final List<AvaliacaoModel> avaliacoes = buscaAvaliacaoSupplier.get();
+		final List<AvaliacaoModel> avaliacoes = executarRequisicao(produtoId);
 
 		return avaliacoes;
 	}
@@ -81,19 +50,7 @@ public class AvaliacaoClientImpl implements AvaliacaoClient {
 			throw e;
 		}
 
-		atualizarCache(produtoId, avaliacoes);
-
 		return Arrays.asList(avaliacoes);
-	}
-
-	private void atualizarCache(Long produtoId, AvaliacaoModel[] avaliacoes) {
-		logger.info("Atualizando cache");
-		CACHE.put(produtoId, Arrays.asList(avaliacoes));
-	}
-
-	private List<AvaliacaoModel> buscarTodosPorProdutoNoCache(Long produtoId, Throwable e) {
-		logger.warn("Buscando avaliações no cache");
-		return CACHE.getOrDefault(produtoId, new ArrayList<>());
 	}
 	
 }
