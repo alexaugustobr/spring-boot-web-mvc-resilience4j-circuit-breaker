f8cb63349dfa6b902554267ce3eac749ea683f20
Alex Silva <alex.silva@algaworks.com>
2022-03-24 17:24:06 -0300

    Mensagens API - sem bucket4j

--- produto/src/main/java/com/algaworks/example/produto/client/avaliacoes/AvaliacaoClient.java
+++
@@ -1,8 +0,0 @@
-package com.algaworks.example.produto.client.avaliacoes;
-
-import java.util.List;
-
-public interface AvaliacaoClient {
-
-	List<AvaliacaoModel> buscarTodosPorProduto(Long productId);
-}
