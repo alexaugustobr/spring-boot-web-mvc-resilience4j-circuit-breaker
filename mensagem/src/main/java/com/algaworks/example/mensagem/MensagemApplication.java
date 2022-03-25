2742f245042eaa8f214cae6a1e40892b458367ce
Alex <alex.silva@algaworks.com.br>
2022-03-25 14:19:05 -0300

    rate - 1

--- mensagem/src/main/java/com/algaworks/example/mensagem/MensagemApplication.java
+++ mensagem/src/main/java/com/algaworks/example/mensagem/MensagemApplication.java
@@ -2,8 +2,10 @@ package com.algaworks.example.mensagem;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
+@EnableCaching
 public class MensagemApplication {
 
 	public static void main(String[] args) {
